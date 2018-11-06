f5f34ebecc73d51b50862363403ea4d3c42f3e24
quh <quhxuxm_quh@163.com>
2018-11-06 08:07:29 +0800

    init

--- entry/src/main/java/online/nwen/server/controller/CommentController.java
+++ entry/src/main/java/online/nwen/server/controller/CommentController.java
@@ -3,7 +3,10 @@ package online.nwen.server.controller;
 import online.nwen.server.executor.api.exception.ExecutorException;
 import online.nwen.server.executor.api.payload.CreateCommentRequestPayload;
 import online.nwen.server.executor.api.payload.CreateCommentResponsePayload;
+import online.nwen.server.executor.api.payload.SearchCommentRequestPayload;
+import online.nwen.server.executor.api.payload.SearchCommentResponsePayload;
 import online.nwen.server.executor.impl.CreateCommentExecutor;
+import online.nwen.server.executor.impl.SearchCommentExecutor;
 import online.nwen.server.service.api.IExecutorService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/comment")
 public class CommentController extends AbstractEntryController {
     private CreateCommentExecutor createCommentExecutor;
+    private SearchCommentExecutor searchCommentExecutor;
 
     CommentController(IExecutorService executorInvoker,
-                      CreateCommentExecutor createCommentExecutor) {
+                      CreateCommentExecutor createCommentExecutor,
+                      SearchCommentExecutor searchCommentExecutor) {
         super(executorInvoker);
         this.createCommentExecutor = createCommentExecutor;
+        this.searchCommentExecutor = searchCommentExecutor;
     }
 
     @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
@@ -28,5 +34,13 @@ public class CommentController extends AbstractEntryController {
             @RequestBody HttpExecutorRequest<CreateCommentRequestPayload> request)
             throws ExecutorException {
         return this.service(request, this.createCommentExecutor, true);
+    }
+
+    @PostMapping(value = "/search", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    HttpExecutorResponse<SearchCommentResponsePayload> search(
+            @RequestBody HttpExecutorRequest<SearchCommentRequestPayload> request)
+            throws ExecutorException {
+        return this.service(request, this.searchCommentExecutor, false);
     }
 }
