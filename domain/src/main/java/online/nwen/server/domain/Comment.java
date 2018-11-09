21051988572e175e0d2e835ff7191e4c9bfb1136
quh <quhxuxm_quh@163.com>
2018-11-09 16:43:47 +0800

    init

--- domain/src/main/java/online/nwen/server/domain/Comment.java
+++ domain/src/main/java/online/nwen/server/domain/Comment.java
@@ -1,6 +1,8 @@
 package online.nwen.server.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +10,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Document(collection = "comments")
+@CompoundIndexes({
+        @CompoundIndex(def = "{'type': 1, 'refDocumentId': 1}")
+})
 public class Comment implements Serializable {
     private static final long serialVersionUID = -8397645527218208099L;
 
