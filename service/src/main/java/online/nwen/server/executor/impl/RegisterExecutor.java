99984f8682c6475a0961426910d10d50c7f18c80
Qu Hao <quhxuxm_quh@163.com>
2018-11-25 11:46:02 +0800

    init

--- service/src/main/java/online/nwen/server/executor/impl/RegisterExecutor.java
+++ service/src/main/java/online/nwen/server/executor/impl/RegisterExecutor.java
@@ -14,23 +14,37 @@ import online.nwen.server.service.api.IAuthorService;
 import online.nwen.server.service.api.ISecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 @Service
 public class RegisterExecutor implements IExecutor<RegisterResponsePayload, RegisterRequestPayload> {
     private static final Logger logger = LoggerFactory.getLogger(RegisterExecutor.class);
     private IAuthorService authorService;
     private IAnthologyService anthologyService;
+    private Pattern nicknamePattern;
+    private Pattern usernamePattern;
+    private Pattern passwordPattern;
 
     public RegisterExecutor(IAuthorService authorService,
-                            IAnthologyService anthologyService) {
+                            IAnthologyService anthologyService,
+                            @Value("${nwen.author-nickname-pattern}")
+                                    Pattern nicknamePattern,
+                            @Value("${nwen.author-username-pattern}")
+                                    Pattern usernamePattern,
+                            @Value("${nwen.author-password-pattern}")
+                                    Pattern passwordPattern) {
         this.authorService = authorService;
         this.anthologyService = anthologyService;
+        this.nicknamePattern = nicknamePattern;
+        this.usernamePattern = usernamePattern;
+        this.passwordPattern = passwordPattern;
     }
 
     @Override
@@ -49,6 +63,18 @@ public class RegisterExecutor implements IExecutor<RegisterResponsePayload, Regi
         if (StringUtils.isEmpty(registerRequestPayload.getNickname())) {
             logger.error("Fail to register author because of the nickname is empty.");
             throw new ExecutorException(ExecutorException.Code.NICKNAME_IS_EMPTY);
+        }
+        if (!this.usernamePattern.matcher(registerRequestPayload.getUsername()).matches()) {
+            logger.error("Fail to register author because of the username format incorrect.");
+            throw new ExecutorException(ExecutorException.Code.USERNAME_FORMAT_INCORRECT);
+        }
+        if (!this.nicknamePattern.matcher(registerRequestPayload.getNickname()).matches()) {
+            logger.error("Fail to register author because of the nickname format incorrect.");
+            throw new ExecutorException(ExecutorException.Code.NICKNAME_FORMAT_INCORRECT);
+        }
+        if (!this.passwordPattern.matcher(registerRequestPayload.getPassword()).matches()) {
+            logger.error("Fail to register author because of the password format incorrect.");
+            throw new ExecutorException(ExecutorException.Code.PASSWORD_FORMAT_INCORRECT);
         }
         logger.debug("Begin to register author with {}", registerRequestPayload);
         if (this.authorService.existsByUsername(registerRequestPayload.getUsername())) {
