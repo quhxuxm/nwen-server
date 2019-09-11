4c685f824db06860418aed059dc497a48c8a2e72
quh <quhxuxm_quh@163.com>
2019-09-11 14:29:44 +0800

    re-init

--- dao/src/main/java/online/nwen/server/dao/impl/SecurityTokenDaoImpl.java
+++ dao/src/main/java/online/nwen/server/dao/impl/SecurityTokenDaoImpl.java
@@ -2,6 +2,9 @@ package online.nwen.server.dao.impl;
 
 import online.nwen.server.dao.api.ISecurityTokenDao;
 import online.nwen.server.domain.SecurityToken;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,17 +20,23 @@ class SecurityTokenDaoImpl implements ISecurityTokenDao {
         this.securityTokenRepository = securityTokenRepository;
     }
 
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "security-token-by-id", key = "#p0.id", condition = "#p0 != null"),
+            @CacheEvict(cacheNames = "security-token-by-token", key = "#p0.token", condition = "#p0 != null")
+    })
     @Transactional
     @Override
     public SecurityToken save(SecurityToken token) {
         return this.securityTokenRepository.save(token);
     }
 
+    @Cacheable(cacheNames = "security-token-by-id", key = "#p0", unless = "#result == null", condition = "#p0 != null")
     @Override
     public SecurityToken getById(Long id) {
         return this.securityTokenRepository.findById(id).orElse(null);
     }
 
+    @Cacheable(cacheNames = "security-token-by-token", key = "#p0", unless = "#result == null", condition = "#p0 != null")
     @Override
     public SecurityToken getByToken(String token) {
         return this.securityTokenRepository.findByToken(token);
@@ -38,6 +47,10 @@ class SecurityTokenDaoImpl implements ISecurityTokenDao {
         return this.securityTokenRepository.findByRefreshableTillBefore(currentTime, pageable);
     }
 
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "security-token-by-id", key = "#p0.id", condition = "#p0 != null"),
+            @CacheEvict(cacheNames = "security-token-by-token", key = "#p0.token", condition = "#p0 != null")
+    })
     @Transactional
     @Override
     public void delete(SecurityToken token) {
